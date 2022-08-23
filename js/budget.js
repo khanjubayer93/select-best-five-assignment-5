b0f08fcd1db735158be28c31fa8c49a8b0d74184
khanjubayer93 <jubayerkhan711@gmail.com>
2022-08-23 23:27:49 +0600

    calculate added

--- js/budget.js
+++ js/budget.js
@@ -3,18 +3,50 @@ document.getElementById('calculate-player-btn').addEventListener('click', functi
     const playerCostElement = document.getElementById('player-input-field');
     const playerCostString = playerCostElement.value;
     const playerCost = parseFloat(playerCostString);
-    const totalPlayerCost = playerCost * 5;
-    playerCostElement.value = totalPlayerCost;
+    playerCostElement.value = playerCost;
 
     const playerExpensesElement = document.getElementById('player-expenses');
     const playerExpensesString = playerExpensesElement.innerText;
     const playerExpenses = parseFloat(playerExpensesString);
+
+
+    const totalPlayerExpenses = playerCost * 5;
     playerExpensesElement.innerText = playerExpenses;
 
-    const totalPlayerExpenses = playerCost
+    return totalPlayerExpenses;
 
 
 
 
-    // console.log(playerInput)
+    // console.log(totalPlayerExpenses)
+})
+
+
+
+document.getElementById('calculate-total').addEventListener('click', function () {
+    // manager 
+    const managerCostField = document.getElementById('manager-input');
+    const managerCostString = managerCostField.value;
+    const managerCost = parseFloat(managerCostString);
+
+    managerCostField.value = managerCost;
+
+
+    // coach 
+    const coachCostField = document.getElementById('coach-input');
+    const coachCostString = coachCostField.value;
+    const coachCost = parseFloat(coachCostString);
+    coachCostField.value = coachCost;
+
+    // final total 
+    const finalTotalElement = document.getElementById('final-total');
+    const finalTotalString = finalTotalElement.innerText;
+    const finalTotal = parseFloat(finalTotalString);
+
+    const fianlExpenses = managerCost + coachCost + finalTotal;
+
+    finalTotalElement.innerText = fianlExpenses;
+
+
+
 })
